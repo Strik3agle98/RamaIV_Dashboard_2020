76552c6ee4c14850d786be7948e5c91c605b0741
Strik3agle98 <can_saelo@hotmail.com>
2021-03-15 18:43:23 +0700

    server now supply individual junction's data

--- server/main.py
+++ server/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import cv2
 import scraper
+import junction
 import io
 from starlette.responses import StreamingResponse
 import numpy as np
@@ -8,10 +10,25 @@ import numpy as np
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-
-@app.get("/image/{id}")
+@app.get("/api/image/{id}")
 async def get_image(id: int):
     image = scraper.get_cctv(id)
     return StreamingResponse(io.BytesIO(image), media_type="image/png")
+
+@app.get("/api/junction/{id}")
+async def get_junction(id: int):
+    return junction.get_junction(id)
