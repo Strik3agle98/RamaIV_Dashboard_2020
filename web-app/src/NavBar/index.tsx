005745b5b1e90808c5a4987d9b26af42ead10cb3
Seth Poophak <42564156+SPRYK@users.noreply.github.com>
2021-05-05 16:05:50 +0700

    add link to all camera page

--- web-app/src/NavBar/index.tsx
+++ web-app/src/NavBar/index.tsx
@@ -31,6 +31,9 @@ const NavBar = () => {
               <Link to="/">Home</Link>
             </Menu.Item>
             <Menu.Item>
+              <Link to="/allcamera">All camera</Link>
+            </Menu.Item>
+            <Menu.Item>
               <Link to="/kepler">HTML Map</Link>
             </Menu.Item>
             <Menu.Item>
